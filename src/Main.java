f6a31152a61eb901b3abc4cd0c437d1cb2e93e07
halil <halilakgun0@gmail.com>
2023-01-27 20:06:06 +0300

    isPalindrome

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(9874789));
+    }
+
+    static boolean isPalindrome(int a) {
+        int reverseNum = 0, temp = a;
+        while (temp != 0) {
+            reverseNum = (reverseNum * 10) + temp % 10;
+            temp /= 10;
+        }
+        return reverseNum == a;
     }
 }
